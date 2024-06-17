5e1bf0f6f20790fea40d45dab5fe7c72ad2ef47c
coderepositor <ilearniexcel@gmail.com>
2024-06-17 15:16:41 +0530

    hotfix changes

--- notes.sql
+++ notes.sql
@@ -307,8 +307,8 @@ c1.first_name + ' ' + c1.last_name as customer1,
 c2.first_name + ' ' + c2.last_name as customer2
 from sales.customers c1 inner join sales.customers c2 
 on c1.customer_id > c2.customer_id and c1.city = c2.city
-order by c1.city,
-c1.first_name
+order by c1.city, 
+c1.first_name desc
 
 select e.ename 'employee', m.ename 'manager' from emp e
 inner join emp m on m.empno = e.mgr
